c5b8174ed77e5ac71158be817c3cbc4b3cab6545
syuki <syu54@icloud.com>
2020-12-03 05:24:17 +0900

    first commit

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from .views import TaskViewSet, UserViewSet, ManageUserView
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet)
+router.register('users', UserViewSet)
+
+
+urlpatterns = [
+    path('myself', ManageUserView.as_view(), name='myself'),
+    path('', include(router.urls)),
+]
